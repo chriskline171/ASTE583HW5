d326c22727eae030ac5aa42d49979dcb5a492879
Chris Kline <chriskline171@hotmail.com>
2025-11-17 21:35:34 -0800

    working version

--- KLINE_ASTE583_HW5_P0.m
+++ KLINE_ASTE583_HW5_P0.m
@@ -9,10 +9,10 @@ Stations.Three.LatDeg  = 35.247164;
 Stations.Three.LonDeg  = 243.205;
 
 %% Load and preprocess raw data
-rawData = load('RawMeasurements.txt');      % [t, stnID, rho_km, rhodot_kmps]
+rawData = load('TruthMeasurements.txt');      % [t, stnID, rho_km, rhodot_kmps]
 rawData(:,3:4) = rawData(:,3:4)*getConstants().Conversions.km2m; % convert to m/m/s
 
-%% Initial OE guess
+%% Initial OE 
 OE0.a     = 10000*getConstants().Conversions.km2m;
 OE0.e     = 0.001;
 OE0.i     = 40*getConstants().Conversions.deg2rad;
@@ -21,6 +21,15 @@ OE0.omega = 40*getConstants().Conversions.deg2rad;
 OE0.theta = 0;
 [r0_ECI, v0_ECI] = PositionVelocityFromOrbitalElements(OE0);
 
+%% A priori data
+sigma_r = 1*getConstants().Conversions.km2m;
+sigma_v = 1;
+P0 = [sigma_r^2*eye(3), zeros(3);
+     zeros(3), sigma_v^2*eye(3)];
+sigma_rho = 1;
+sigma_rhoDot = 1*getConstants().Conversions.mm2m;
+R = [sigma_rho^2, 0;
+    0, sigma_rhoDot^2];
 %% Build augmented initial state (r,v,STM)
 X0 = [r0_ECI; v0_ECI; reshape(eye(6),36,1)];
 
@@ -34,13 +43,14 @@ options = odeset('RelTol',1e-10,'AbsTol',1e-10);
 phi_G0 = 170*getConstants().Conversions.deg2rad;
 
 %% Batch filter (Gauss–Newton on initial state)
-delta_x = 0;            % correction vector
+delta_x_hat = zeros(6,1);            % correction vector
+delta_x_bar = delta_x_hat;
 firtIteration = true;
 kk = 0;
-
-while max(abs(delta_x))>1e-3 || firtIteration
+maxIterLimit = 10;
+while delta_x_hat'*(P0\delta_x_hat)>1e-15 || firtIteration
     firtIteration = false;
-    X0(1:6) = X0(1:6) + delta_x;      % update initial state guess
+    X0(1:6) = X0(1:6) + delta_x_hat;      % update initial state guess
 
     [t,X] = ode45(@twobodyEOMJ2WithSTM, tspan, X0, options);
 
@@ -63,8 +73,8 @@ while max(abs(delta_x))>1e-3 || firtIteration
     nn = 1;
     rawDataStartIndx = find(rawData(:,1)>=t0,1,'first')-1;
 
-    HTransposeH = zeros(6,6);
-    HTransposeDeltaY = zeros(6,1);
+    InfoMat = inv(P0);
+    InfoVec = P0\delta_x_bar;
 
     %% Loop over measurement points
     for ii = indxStart:indxEnd
@@ -73,32 +83,36 @@ while max(abs(delta_x))>1e-3 || firtIteration
         delta_y(nn,2) = stnID;
 
         % modelled measurement
-        [rho,~,~,rhoDot,~,~] = ...
-            groundsStationOutput(Stations.(stationNames{stnID}), ...
-                                 X(ii,:)', phi_G(ii));
+        [rho,~,~,rhoDot,~,~] = groundsStationOutput(Stations.(stationNames{stnID}),X(ii,:)', phi_G(ii));
 
         % residuals
         delta_y(nn,3) = rawData(rawDataStartIndx+nn,3) - rho;
         delta_y(nn,4) = rawData(rawDataStartIndx+nn,4) - rhoDot;
 
         % measurement sensitivity wrt current (r,v)
-        Htld = calculateH_rho_rhoDot_wrt_r_v( ...
-                 X(ii,:)', Stations.(stationNames{stnID}), phi_G(ii));
+        Htld = calculateH_rho_rhoDot_wrt_r_v(X(ii,:)', Stations.(stationNames{stnID}), phi_G(ii));
 
         % STM at this time
         Phi = reshape(X(ii,7:end),6,6);
         H = Htld * Phi;    % sensitivity wrt initial state
-
+        
         % accumulate normal eqns
-        HTransposeH      = HTransposeH      + H'*H;
-        HTransposeDeltaY = HTransposeDeltaY + H'*delta_y(nn,3:4)';
+        InfoMat = InfoMat + H'*(R\H);
+        InfoVec = InfoVec + H'*(R\delta_y(nn,3:4)');
 
         nn = nn + 1;
     end
 
-    delta_x = HTransposeH \ HTransposeDeltaY;   % Gauss-Newton update
+    delta_x_hat = InfoMat \ InfoVec;   % Gauss-Newton update
+    delta_x_bar = delta_x_bar - delta_x_hat; % Update a priori state deviation
     kk = kk + 1;
+    if kk>maxIterLimit
+         warning(['Batch filter did not converge within %d iterations.\n' ...
+             'Last step Mahalanobis norm: %.3e\n'], ...
+             maxIterLimit, delta_x_hat'*(P0\delta_x_hat));
+        break
+    end
 end
-
+kk
 %% Final post-fit difference from OE-based initial state
 delta_x_postfit = X0(1:6) - [r0_ECI; v0_ECI];
