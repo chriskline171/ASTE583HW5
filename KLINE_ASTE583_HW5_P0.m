524c77e74b5c8a2681766dcbf0adf8534ed33800
Chris Kline <chriskline171@hotmail.com>
2025-11-22 09:53:35 -0800

    change var name to measData

--- KLINE_ASTE583_HW5_P0.m
+++ KLINE_ASTE583_HW5_P0.m
@@ -11,8 +11,8 @@ Stations.Three.LatDeg  = 35.247164;
 Stations.Three.LonDeg  = 243.205;
 
 %% Load and preprocess raw data
-rawData = load('TruthMeasurements.txt');      % [t, stnID, rho_km, rhodot_kmps]
-rawData(:,3:4) = rawData(:,3:4)*getConstants().Conversions.km2m; % convert to m/m/s
+measData = load('TruthMeasurements.txt');      % [t, stnID, rho_km, rhodot_kmps]
+measData(:,3:4) = measData(:,3:4)*getConstants().Conversions.km2m; % convert to m/m/s
 
 %% Initial OE 
 OE0.a     = 10000*getConstants().Conversions.km2m;
@@ -39,7 +39,7 @@ t0 = 0;
 tf = 24*getConstants().Conversions.hr2s;
 
 % force ode45 output at measurement times
-tspan = [t0, rawData((rawData(:,1)<tf & rawData(:,1)>t0),1)', tf];
+tspan = [t0, measData((measData(:,1)<tf & measData(:,1)>t0),1)', tf];
 
 options = odeset('RelTol',1e-10,'AbsTol',1e-10);
 phi_G0 = 170*getConstants().Conversions.deg2rad;
@@ -63,12 +63,12 @@ while MalhonbisDist>1e-10 || firstIteration
     [t,X] = ode45(@twobodyEOMJ2WithSTM, tspan, X0, options);
 
     % determine if endpoints contain measurements
-    if any(t0 == rawData(:,1))
+    if any(t0 == measData(:,1))
         indxStart = 1;
     else
         indxStart = 2;
     end
-    if any(tf == rawData(:,1))
+    if any(tf == measData(:,1))
         indxEnd = length(t);
     else
         indxEnd = length(t)-1;
@@ -80,7 +80,7 @@ while MalhonbisDist>1e-10 || firstIteration
     stationNames = fieldnames(Stations);
 
     nn = 1;
-    rawDataStartIndx = find(rawData(:,1)>=t0,1,'first')-1;
+    rawDataStartIndx = find(measData(:,1)>=t0,1,'first')-1;
 
     InfoMat = inv(P0);
     InfoVec = P0\delta_x_bar;
@@ -88,15 +88,15 @@ while MalhonbisDist>1e-10 || firstIteration
     %% Loop over measurement points
     for ii = indxStart:indxEnd
         delta_y(nn,1) = t(ii);
-        stnID = rawData(rawDataStartIndx+nn,2);
+        stnID = measData(rawDataStartIndx+nn,2);
         delta_y(nn,2) = stnID;
 
         % modelled measurement
         [rho,~,~,rhoDot,~,~] = groundsStationOutput(Stations.(stationNames{stnID}),X(ii,:)', phi_G(ii));
 
         % residuals
-        delta_y(nn,3) = rawData(rawDataStartIndx+nn,3) - rho;
-        delta_y(nn,4) = rawData(rawDataStartIndx+nn,4) - rhoDot;
+        delta_y(nn,3) = measData(rawDataStartIndx+nn,3) - rho;
+        delta_y(nn,4) = measData(rawDataStartIndx+nn,4) - rhoDot;
 
         % measurement sensitivity wrt current (r,v)
         Htld = calculateH_rho_rhoDot_wrt_r_v(X(ii,:)', Stations.(stationNames{stnID}), phi_G(ii));
